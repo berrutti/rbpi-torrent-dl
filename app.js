d246cb786518931e9a49c1bd48632d3ea475770a
Matias Berrutti <berrutti@gmail.com>
2020-06-16 20:09:35 -0300

    - Add "temp" dir to gitignore - Remove global variables. Every operation is self contained - Add code to select files to download (with workaround) - Move templates to pug

--- app.js
+++ app.js
@@ -8,54 +8,65 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'pug');
 
-let magnetURI = '';
+const tempPath = path.join(__dirname, 'temp');
+const targetPath = path.join(__dirname, '..', 'toMove');
 
 const cleanTempFolder = () => {
-    console.log('Cleaning temp folder')
     try {
-        rimraf.sync('./temp/*');
-        console.log('Done');
+        rimraf.sync(tempPath);
     } catch (e) {
         console.log('Could not clean the temp folder');
         console.error(e);
     }
 }
 
+const stopDownload = (torrent) => {
+    torrent.files.forEach(file => file.deselect());
+    torrent.deselect(0, torrent.pieces.length - 1, false);
+}
+
+const moveFiles = () => {
+    console.log('Moved files inside', tempPath, 'to', targetPath);
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const files = [];
-
 app.post('/magnet', (req, res) => {
-    magnetURI = req.body['magnetURI'];
-
-    client.add(magnetURI, { path: './temp/' }, (torrent) => {
+    const magnetURI = req.body['magnetURI'];
+    client.add(magnetURI, { path: tempPath }, (torrent) => {
+        const files = [];
         torrent.files.forEach(file => {
             files.push(file.name);
         });
         client.remove(magnetURI);
         cleanTempFolder();
-        res.redirect('files');
+        res.render('files', { files, magnetURI });
     });
 });
 
-app.get('/files', (req, res) => {
-    res.render('files', { files });
-});
-
 app.post('/download', (req, res) => {
-    files = req.body['files'];
-    client.add(magnetURI, { path: './temp/' }, (torrent) => {
-        console.log('Started download of:', torrent.infoHash);
-        torrent.on('download', (data) => {
-            res.send(data);
-        })
-        client.remove(magnetURI);
-        cleanTempFolder();
+    const magnetURI = req.body['magnetURI'];
+    client.add(magnetURI, { path: tempPath }, (torrent) => {
+        console.log('Started download of torrent with hash:', torrent.infoHash);
+        res.send('<h1>Downloading files</h1>');
+        stopDownload(torrent);
+        torrent.files.forEach((file) => {
+            if (req.body[file.name] === 'on') {
+                console.log('Selecting file:', file.name);
+                file.select();
+            }
+        });
+
+        torrent.on('done', function () {
+            console.log('Torrent finished downloading.');
+            moveFiles();
+            cleanTempFolder();
+        });
     });
 });
 
 app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index');
 });
 
 app.listen(3000);
